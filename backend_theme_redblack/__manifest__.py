55eb9b38e04096cf2ca229e9bb710797b65ff34d
Pablo Androetto <androettop@gmail.com>
2020-08-06 15:47:22 -0300

    redblack theme

---
+++ backend_theme_redblack/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Red and black backend theme",
+    'summary': "Red and black backend theme",
+    'description': "Red and black backend theme",
+    'author': "Pablo Androetto",
+    'website': "http://www.pabloandroetto.com",
+    'images': ['static/description/banner.png','static/description/view_screenshot.png'],
+    'category': 'Theme',
+    'version': '1.0',
+    'price': 49.99,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'installable': True,
+    'depends': ['web','web_responsive'],
+    'data': [
+        'views/views.xml',
+    ],
+}
