8ded2f533f6a70b4d5a9730640e33615c5e80dc8
Pablo Androetto <androettop@gmail.com>
2020-08-06 14:32:32 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "backend_theme_bluegreen",
+    'summary': "Blue green backend theme",
+    'description': "Blue green backend theme",
+    'author': "Pablo Androetto",
+    'website': "http://www.yourcompany.com",
+    'category': 'Theme',
+    'version': '0.1',
+    'price': 49.99,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'depends': ['web_responsive'],
+    'data': [
+        'views/views.xml',
+    ]
+}
